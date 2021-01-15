a0daaa7697b480034b2c10b5b01689e79a2e3010
samsuzzaman1998 <64584061+samsuzzaman1998@users.noreply.github.com>
2021-01-15 21:58:48 +0600

    version 1.0(upload)

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// =======================================
+// JQUERY READY FUNCTION WITH STRICT MODE
+// ========================================
+(function ($) {
+    "use strict";
+    // all jquery codes are written from below=========
+
+
+})(jQuery);
